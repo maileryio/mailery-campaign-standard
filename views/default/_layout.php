569f7ea6ed1ba3873504b75dc93c4008cafe1afc
Igor Maliy <imaliy.filsh@gmail.com>
2022-07-18 00:07:04 +0300

    Development

--- views/default/_layout.php
+++ views/default/_layout.php
@@ -4,18 +4,24 @@ use Mailery\Activity\Log\Widget\ActivityLogLink;
 use Mailery\Icon\Icon;
 use Mailery\Widget\Link\Link;
 use Mailery\Web\Widget\DateTimeFormat;
+use Mailery\Widget\Highcharts\Highcharts;
 use Yiisoft\Yii\Bootstrap5\Nav;
 
 /** @var Yiisoft\Yii\WebView $this */
 /** @var Psr\Http\Message\ServerRequestInterface $request */
 /** @var Mailery\Campaign\Counter\RecipientCounter $recipientCounter */
 /** @var Mailery\Campaign\Standard\Entity\StandardCampaign $campaign */
+/** @var Yiisoft\Translator\TranslatorInterface $translator */
 /** @var Yiisoft\Yii\View\Csrf $csrf */
 
 $this->setTitle($campaign->getName());
 
 $status = $campaign->getStatus();
 $sendout = $campaign->getLastDefaultSendout();
+
+if ($sendout !== null) {
+    $recipientCounter = $recipientCounter->withSendout($sendout);
+}
 
 ?><div class="row">
     <div class="col-12">
@@ -28,10 +34,8 @@ $sendout = $campaign->getLastDefaultSendout();
                             Changed at <?= DateTimeFormat::widget()->dateTime($campaign->getUpdatedAt()) ?>
                         </p>
                         <?php if ($sendout !== null) { ?>
-                            <?php $recipientCounter = $recipientCounter->withSendout($sendout); ?>
-
-                            <p class="mt-1 mb-0 small">
-                                Sent on <?= DateTimeFormat::widget()->dateTime($sendout->getCreatedAt()) ?> to <b><?= $recipientCounter->getSentCount() ?></b> <span class="text-muted">/ <?= $recipientCounter->getTotalCount() ?></span> subscribers
+                            <p class="mt-3 mb-0">
+                                Sent on <?= DateTimeFormat::widget()->dateTime($sendout->getCreatedAt()) ?> to <b><?= $recipientCounter->getSentCount() ?></b> / <?= $recipientCounter->getTotalCount() ?> subscribers
                                 <?= Icon::widget()
                                     ->name('help-circle-outline')
                                     ->options([
@@ -121,15 +125,226 @@ $sendout = $campaign->getLastDefaultSendout();
                     </div>
                 <?php } else if($sendout !== null) { ?>
                     <?php if ($sendout->getStatus()->isErrored()) { ?>
-                    <div class="mb-4"></div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="alert alert-danger" role="alert">
-                                <?= $sendout->getError(); ?>
+                        <div class="mb-4"></div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="alert alert-danger" role="alert">
+                                    <?= $sendout->getError(); ?>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     <?php } ?>
+
+                    <div class="mb-4"></div>
+                    <div class="row">
+                        <div class="col-md-5">
+                            <?= Highcharts::widget()
+                                ->options([
+                                    'chart' => [
+                                        'type' => 'pie',
+                                        'height' => 300,
+                                    ],
+                                    'title' => [
+                                        'text' => '<small><b>Top 10 countries</b></small>',
+                                        'verticalAlign' => 'bottom',
+                                        'useHTML' => true,
+                                    ],
+                                    'tooltip' => [
+                                        'headerFormat' => '',
+                                        'pointFormat' => '{point.custom.tooltip}',
+                                    ],
+                                    'credits' => [
+                                        'enabled' => false,
+                                    ],
+                                    'plotOptions' => [
+                                        'pie' => [
+                                            'size' => 200,
+                                            'allowPointSelect' => true,
+                                            'cursor' => 'pointer',
+                                            'dataLabels' => [
+                                                'enabled' => true,
+                                                'format' => '<b>{point.name}</b>: {point.percentage:.1f} %',
+                                            ],
+                                        ],
+                                    ],
+                                    'series' => [
+                                        [
+                                            'type' => 'pie',
+                                            'name' => 'Countries',
+                                            'data' => [
+                                                [
+                                                    'name' => 'United States',
+                                                    'y' => 117,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 1]),
+                                                    ],
+                                                    'sliced' => true,
+                                                    'selected' => true,
+                                                ],
+                                                [
+                                                    'name' => 'Not defined',
+                                                    'y' => 33,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 33]),
+                                                    ],
+                                                ],
+                                                [
+                                                    'name' => 'United Kingdom',
+                                                    'y' => 33,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 33]),
+                                                    ],
+                                                ],
+                                                [
+                                                    'name' => 'United Kingdom',
+                                                    'y' => 33,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 33]),
+                                                    ],
+                                                ],
+                                                [
+                                                    'name' => 'United Kingdom',
+                                                    'y' => 33,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 33]),
+                                                    ],
+                                                ],
+                                                [
+                                                    'name' => 'United Kingdom',
+                                                    'y' => 33,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 33]),
+                                                    ],
+                                                ],
+                                                [
+                                                    'name' => 'United Kingdom',
+                                                    'y' => 33,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 33]),
+                                                    ],
+                                                ],
+                                                [
+                                                    'name' => 'United Kingdom',
+                                                    'y' => 33,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 33]),
+                                                    ],
+                                                ],
+                                                [
+                                                    'name' => 'United Kingdom',
+                                                    'y' => 33,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 33]),
+                                                    ],
+                                                ],
+                                                [
+                                                    'name' => 'United Kingdom',
+                                                    'y' => 33,
+                                                    'custom' => [
+                                                        'tooltip' => $translator->translate('<b>{name}</b>: {count, number} {count, plural, one{contact} other{contacts}}', ['name' => 'United Kingdom', 'count' => 33]),
+                                                    ],
+                                                ],
+                                            ],
+                                        ],
+                                    ],
+                                ]); ?>
+                        </div>
+                        <div class="col-md-7">
+                            <?= Highcharts::widget()
+                                ->options([
+                                    'chart' => [
+                                        'type' => 'bar',
+                                        'height' => 300,
+                                    ],
+                                    'title' => [
+                                        'text' => '<small><b>Activity</b></small>',
+                                        'verticalAlign' => 'bottom',
+                                        'useHTML' => true,
+                                    ],
+                                    'xAxis' => [
+                                        'visible' => false,
+                                    ],
+                                    'yAxis' => [
+                                        'title' => '',
+                                        'min' => 0,
+                                        'allowDecimals' => false,
+                                    ],
+                                    'tooltip' => [
+                                        'headerFormat' => '',
+                                        'pointFormat' => '{series.name}: {point.y}',
+                                    ],
+                                    'credits' => [
+                                        'enabled' => false,
+                                    ],
+                                    'plotOptions' => [
+                                        'bar' => [
+                                            'borderWidth' => 0,
+                                            'shadow' => false,
+                                            'groupPadding' => 0,
+                                            'cursor' => 'pointer',
+                                            'dataLabels' => [
+                                                'enabled' => true,
+                                                'style' => [
+                                                    'fontWeight' => 'normal',
+                                                    'color' => "#797979",
+                                                ],
+                                            ],
+                                        ],
+                                    ],
+                                    'series' => [
+                                        [
+                                            'name' => 'Recipients',
+                                            'data' => [
+                                                $recipientCounter->getSentCount(),
+                                            ],
+                                            'color' => '#6c757d',
+                                        ],
+                                        [
+                                            'name' => 'Opened',
+                                            'data' => [
+                                                $recipientCounter->getOpenedCount(),
+                                            ],
+                                            'color' => '#28a745',
+                                        ],
+                                        [
+                                            'name' => 'Unopened',
+                                            'data' => [
+                                                $recipientCounter->getSentCount() - $recipientCounter->getOpenedCount(),
+                                            ],
+                                            'color' => '#ffc107',
+                                        ],
+                                        [
+                                            'name' => 'Clicked',
+                                            'data' => [
+                                                $recipientCounter->getClickedCount(),
+                                            ],
+                                            'color' => '#007bff',
+                                        ],
+                                        [
+                                            'name' => 'Unsubscribed',
+                                            'data' => [
+                                                $recipientCounter->getUnsubscribedCount(),
+                                            ],
+                                            'color' => '#dc3545',
+                                        ],
+                                        [
+                                            'name' => 'Bounced',
+                                            'data' => [
+                                                $recipientCounter->getBouncedCount(),
+                                            ],
+                                            'color' => '#343a40',
+                                        ],
+                                        [
+                                            'name' => 'Marked as spam',
+                                            'data' => [
+                                                $recipientCounter->getComplainedCount(),
+                                            ],
+                                            'color' => '#343a40',
+                                        ],
+                                    ],
+                                ]); ?>
+                        </div>
+                    </div>
                 <?php } ?>
             </div>
         </div>
